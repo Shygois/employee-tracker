3fed4b4956333f26d08f85cd7fca68542c5ed94b
Shygois <shayana.gois@att.net>
2021-04-24 17:00:23 -0700

    set up view departments and add departments

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const inquirer = require("inquirer");
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'tracker_db'
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'tracker_db'
 });
 
 // simple query
@@ -27,12 +27,158 @@ const mainMenu = () => {
             type: 'list',
             name: 'option',
             message: 'What would you like to do?',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role', 'Quit']
         }
     ])
-    .then((response) => {
-        console.log('You selected ' + response.option + '.')
-    })
+        .then((response) => {
+            console.log('You selected ' + response.option + '.')
+
+            switch (response.option) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+
+                case 'View all roles':
+                    console.log('View all roles');
+                    break;
+
+                case 'View all employees':
+                    console.log('View all employees');
+                    break;
+
+                case 'Add a department':
+                    addDepartment();
+                    break;
+
+                case 'Add a role':
+                    console.log('Add a role');
+                    break;
+
+                case 'Add an employee':
+                    console.log('Add an employee');
+                    break;
+
+                case 'Update employee role':
+                    updateEmployeeRole();
+                    break;
+
+                default:
+                    console.log('Goodbye!');
+                    connection.end();
+            }
+        })
+};
+
+const viewDepartments = () => {
+    connection.query(
+        'SELECT * FROM `department`',
+        (err, results) => {
+            if (err) throw err;
+            console.table(results);
+            mainMenu();
+        }
+    );
+};
+
+
+const addDepartment = () => {
+    inquirer.prompt(
+        {
+            type: "input",
+            name: "department",
+            message: "Please enter department name.",
+            validate: departmentInput => {
+                if (departmentInput) {
+                    return true;
+                } else {
+                    console.log('You must specify a department name!');
+                    return false;
+                }
+            }
+        }
+    )
+        .then(({ department }) => {
+            connection.query(
+                `INSERT INTO department(name) VALUES ('${department}')`,
+                (err) => {
+                    if (err) throw err;
+                    viewDepartments();
+                }
+            );
+        })
+};
+
+// TODO: View all roles
+// TODO: Add a role
+
+// TODO: View all employees
+// TODO: Add an employee
+
+// TODO: Update employee role
+const updateEmployeeRole = () => {
+    connection.query(
+        'SELECT * FROM `employee`',
+        (err, results) => {
+            if (err) throw err;
+            console.table(results)
+            let employees = results.map(employee => {
+                console.log(employee.first_name);
+                console.log(employee.last_name);
+                return {
+                    name: employee.first_name + ' ' + employee.last_name, 
+                    value: employee.id
+                };
+            });
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employeeId',
+                    message: 'Which employee would you like to update?',
+                    choices: employees
+                }
+            ])
+            .then(({ employeeId }) => {
+                let employee_id = (employeeId);
+                connection.query(
+                    'SELECT * FROM `role`',
+                    (err, results) => {
+                        if (err) throw err;
+                        console.table(results)
+                        let roles = results.map(role => {
+                            console.log(role.title);
+                            console.log(role.salary);
+                            console.log(role.department_id);
+                            return {
+                                name: role.title, 
+                                value: role.id
+                            };
+                        });
+                        inquirer.prompt([
+                            {
+                                type: 'list',
+                                name: 'roleId',
+                                message: 'Which role would you like to assign to this employee?',
+                                choices: roles
+                            }
+                        ])
+                        .then(({ roleId }) => {
+                            connection.query(
+                                `UPDATE employee 
+                                SET 
+                                    role_id = ${roleId}
+                                WHERE
+                                    id = ${employee_id};`,
+                                (err, results) => {
+                                    if (err) throw err;
+                                    // TODO: Implement viewEmployees();
+                                }
+                            )
+                        }) 
+                    }
+                )
+            })
+        }
+    );
 };
 
 mainMenu();
